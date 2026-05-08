6f54b5222799acb3edd170678a8e65b131074b5c
Plastic-time <ljiaqi1234@gmail.com>
2026-05-08 01:30:18 -0400

    Add country and type dictionaries

---
+++ dict/country_code.js
@@ -0,0 +1,15 @@
+module.exports = {
+  vehicle_type: ["ground", "aviation", "helicopters", "ships", "boats"],
+  country_code: [
+    "usa",
+    "germany",
+    "ussr",
+    "britain",
+    "japan",
+    "china",
+    "italy",
+    "france",
+    "sweden",
+    "israel",
+  ],
+};
